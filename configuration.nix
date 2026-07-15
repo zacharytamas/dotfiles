0093c246d7e56c0895e87924d32bfc5a9b603271
Zachary <zachary@tamas.sh>
2026-07-15 14:58:58 -0400

    configuring macos

--- configuration.nix
+++ configuration.nix
@@ -8,4 +8,16 @@
 
   system.primaryUser = "zacharyjones";
   system.stateVersion = 6;
+
+  system.defaults = {
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      KeyRepeat = 2;
+      InitialKeyRepeat = 15;
+    };
+    dock.autohide = true;
+    finder.FXPreferredViewStyle = "Nlsv";
+    finder.CreateDesktop = false;
+    trackpad.Clicking = true;
+  };
 }
