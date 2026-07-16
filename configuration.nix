ac1b8e651fe86ce9d71ab0a90211a3cbe9b00590
Zachary <zachary@tamas.sh>
2026-07-16 12:49:09 -0400

    further configuration, learning nix/nix-darwin

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ user, ... }:
 
 {
   nix.enable = false;
@@ -6,7 +6,8 @@
   nixpkgs.config.allowUnfree = true;
   nixpkgs.hostPlatform = "aarch64-darwin"; 
 
-  system.primaryUser = "zacharyjones";
+  system.primaryUser = user;
+  users.users.${user} = { home = "/Users/${user}"; };
   system.stateVersion = 6;
 
   system.defaults = {
@@ -14,10 +15,50 @@
       AppleInterfaceStyle = "Dark";
       KeyRepeat = 2;
       InitialKeyRepeat = 15;
+      NSNavPanelExpandedStateForSaveMode = true;
+      AppleICUForce24HourTime = true;
     };
     dock.autohide = true;
     finder.FXPreferredViewStyle = "Nlsv";
     finder.CreateDesktop = false;
     trackpad.Clicking = true;
+  };
+
+  nix-homebrew = {
+    enable = true;
+    user = user;
+    enableRosetta = true;
+  };
+
+  homebrew = {
+    enable = true;
+    onActivation.cleanup = "zap";
+    onActivation.autoUpdate = true;
+    onActivation.extraFlags = [ "--force" ];
+    taps = [
+      { name = "vet-run/homebrew-vet"; trusted = true; }
+      { name = "derailed/k9s"; trusted = true; }
+      { name = "weknowtraining/tap"; trusted = true; }
+      { name = "humanlayer/humanlayer"; trusted = true; }
+    ];
+    brews = [
+      "herdr"
+      "mise"
+      "fzf"
+      "zoxide"
+      "dua-cli"
+      "sk"
+      "tree"
+      "vet-run"
+      "k9s"
+      "wkt"
+    ];
+    casks = [
+      "humanlayer"
+      "1password-cli"
+      "codexbar"
+      "font-maple-mono"
+      "font-maple-mono-nf"
+    ];
   };
 }
--- flake.nix
+++ flake.nix
@@ -5,11 +5,31 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
     nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }: {
-    darwinConfigurations."wkt" = nix-darwin.lib.darwinSystem {
-      modules = [ ./configuration.nix ];
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager }: 
+    let 
+      user = "zacharyjones";
+    in
+    {
+      darwinConfigurations."wkt" = nix-darwin.lib.darwinSystem {
+        specialArgs = { inherit user; };
+        modules = [ 
+          ./configuration.nix 
+          nix-homebrew.darwinModules.nix-homebrew
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit user; };
+            home-manager.users.${user} = import ./home.nix;
+          }
+        ];
+      };
     };
-  };
 }
