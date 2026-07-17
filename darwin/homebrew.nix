2bbbd6b247d3daabc90f4d7fd09f6484849cea11
Zachary <zachary@tamas.sh>
2026-07-16 22:26:32 -0400

    add mosh and moshi-hook

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -16,11 +16,14 @@
       { name = "derailed/k9s"; trusted = true; }
       { name = "weknowtraining/tap"; trusted = true; }
       { name = "humanlayer/humanlayer"; trusted = true; }
+      { name = "rjyo/moshi"; trusted = true; }
     ];
 
     brews = [
       "herdr"
       "mise"
+      "mosh"
+      { name = "moshi-hook"; start_service = true; }
       "fzf"
       "zoxide"
       "dua-cli"
