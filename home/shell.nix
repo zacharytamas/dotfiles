2cd423b67c3d406556237c7705c82e8ea2f6d81b
Zachary <zachary@tamas.sh>
2026-08-25 13:09:42 -0400

    feat(home): Added bun binaries to shell path
    - Included $HOME/.bun/bin in home.sessionPath so bun-installed tools resolve.

--- home/shell.nix
+++ home/shell.nix
@@ -1,6 +1,6 @@
 { ... }: {
 
-  home.sessionPath = [ "$HOME/.local/bin" "$HOME/.cargo/bin" ];
+  home.sessionPath = [ "$HOME/.local/bin" "$HOME/.cargo/bin" "$HOME/.bun/bin" ];
   home.sessionVariables = {
     EDITOR = "nvim";
     XDG_CONFIG_HOME = "$HOME/.config";
